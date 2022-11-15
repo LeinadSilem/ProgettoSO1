c5ce79ccc4d32c0575111431d0e49a1995132a21
LeinadSilem <d.melis51@studenti.unica.it>
2022-11-15 11:31:51 +0100

    Primo Commit
    Creazione dei 4 file necessari, anche se probabilmente dovrò creare almeno un'altra libreria, solo perchè se mi fanno un'altra storia di compilazione non intelligente smatto

---
+++ gameLib.h
@@ -0,0 +1,39 @@
+#ifndef GATCHA_CROSSY_ROAD
+#define GATCHA_CROSSY_ROAD
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef enum {RED,YELLOW,GREEN,CYAN,BLUE,MAGENTA,WHITE}Color;
+typedef enum {UP,DOWN,LEFT,RIGHT}Direction;
+typedef enum {PHROG,SPIDER,CAR,LOG}EntityType;
+
+typedef struct position
+{
+	int x;
+	int y;
+}Position;
+
+typedef struct hitbox
+{
+	Position topleft;
+	Position botright;
+}Hitbox;
+
+typedef struct data
+{
+	Color c;
+	Direction d;
+	EntityType et;
+	Hitbox h;
+	int lives;
+}Data;
+
+typedef struct entity
+{
+	Data data;
+	Data *container;
+}Entity;
+
+#endif
